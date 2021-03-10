238b644f9c7623e5d45fa3de942a8c4d7225499f
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-03-10 16:45:14 +0100

    Modify file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tomisin\'s deployment of a Nodejs App on Heroku with Jenkins' });
 });
 
 module.exports = router;
